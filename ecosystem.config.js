35dbb23e3eec6d57e2781390479766f6b1644c3d
Alex Reichert <reichertjalex@gmail.com>
2017-10-20 11:40:52 -0700

    Set up pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'aws-nginx-tutorial',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-220-225-49.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/tutorial.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/reichert621/aws-nginx-tutorial.git',
+      path: '/home/ubuntu/aws-nginx-tutorial',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
